433a3b4ecd1328f4892c343aab744a83ee260298
shamshath begum <sham29.b@gmail.com>
2023-09-01 20:28:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import DisplayLeads from "./components/DisplayLeads";
+import Email from "./components/Email";
+import ForgotPassword from "./components/ForgotPassword";
+import Header from "./components/Header";
+import LeadInfo from "./components/LeadInfo";
+import Login from "./components/Login";
+import ManageLead from "./components/ManageLead";
+import PasswordReset from "./components/PasswordReset";
+import SignUp from "./components/SignUp";
+import Home from "./homePage/Home";
 
+export const url = "http://localhost:8000";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/users/forgotpassword/:id/:token"
+            element={<ForgotPassword />}
+          />
+          <Route path="/users/passwordreset" element={<PasswordReset />} />
+          <Route path="/users/signup" element={<SignUp />} />
+          <Route path="/manage-lead/:id" element={<ManageLead />} />
+          <Route path="/display-lead" element={<DisplayLeads />} />
+          <Route path="/lead-info" element={<LeadInfo />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/users/login-user" element={<Login />} />
+          <Route path="/email" element={<Email />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/incoming-leads" element={<Dashboard />} />
+          <Route path="/converted-leads" element={<Dashboard />} />
+          <Route path="/closed-leads" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/users/login-user" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
+
+
+
---
+++ src/charts/PieChart.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { url } from "../App";
+
+import axios from "axios";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+function PieChart() {
+  const [listdata, setListdata] = useState([]);
+
+  let token = sessionStorage.getItem("token");
+
+  let getData = async () => {
+    let res = await axios.get(`${url}/chart-details`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    setListdata(res.data.leads);
+  };
+
+  useEffect(() => {
+    if (token) {
+      getData();
+    }
+  }, [token]);
+
+  return (
+    <>
+      <LineChart
+        width={500}
+        height={300}
+        data={listdata}
+        margin={{
+          top: 6,
+          right: 30,
+          left: 20,
+          bottom: 6,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="_id" />
+        <YAxis dataKey="count" />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="count"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    </>
+  );
+}
+
+export default PieChart;
